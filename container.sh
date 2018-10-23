31201fdff114ed1eab2677625750fb64b4171386
katchie <onwobia@appzonegroup.com>
2018-10-23 17:02:26 +0100

    edit variable name

--- container.sh
+++ container.sh
@@ -1,7 +1,7 @@
 docker ps -aq -f status=exited
-result = $( docker ps -aq -f status=exited )
+RESULT = "$( docker ps -aq -f status=exited )"
 
-if [ "$result" == " " ]; then 
+if [ "$RESULT" == " " ]; then 
   echo "No containers to remove"
 else
 docker rm $(docker ps --all -q -f status=exited)
