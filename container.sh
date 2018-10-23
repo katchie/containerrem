98a9d813c3c8b11467be8f4aa974c4e995bf7218
katchie <onwobia@appzonegroup.com>
2018-10-23 14:23:51 +0100

    first commit

---
+++ container.sh
@@ -0,0 +1,11 @@
+docker ps -aq -f status=exited
+result = $( docker ps -aq -f status=exited )
+
+if [ "$result" == " " ]; then 
+  echo "No containers to remove"
+else
+docker rm $(docker ps --all -q -f status=exited)
+
+  fi
+}
+
